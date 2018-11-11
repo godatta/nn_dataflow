7f571d272353ac3df8c03a182c167bd17a244611
Vikram <msharmavikram@gmail.com>
2018-11-11 12:57:40 -0600

    updated to work in the machine

--- tracegen/generate_patterns.py
+++ tracegen/generate_patterns.py
@@ -2,7 +2,7 @@ import random as rand
 
 #Variables:
 
-num_pattern  = 10
+num_pattern  = 300000 #maximum number of images you can store in 4GB assuming each image is 36KB
 start_time   = 5000.112 #us
 end_time     = 6000 #us
 
@@ -18,12 +18,13 @@ img_numpages   = 3
 #SSD Related parameters:
 
 #32 channels, 8 dies, 2 planes, 4K blocks, 256 pages per block and each page size is 4096
-#32×8×2×4096×256×4096÷(1024×1024×1024)
+#32×8×2×4096×256×4096/(1024×1024×1024)
 # this gives 2TB
 
 page_read_time = 16 #in us
 ssd_block_start= 0
-ssd_block_end  = 32*8*2*4096*256 #number of pages - 4 M
+#ssd_block_end  = 32*8*2*4096*256 #number of pages - 4 M
+ssd_block_end  = 4096*256 #number of pages - 4 M
 ssd_page_size  = 4096           #in bytes
 ssd_numdev     = 1
 ssd_mode       = 1 #0 is write, 1 is read
@@ -38,6 +39,6 @@ for i in range(0, num_pattern):
     ppn_start = rand.randint(ssd_block_start, ssd_block_end)
     for p in range(0, img_numpages):
         start_time = round(start_time + rand.uniform(0.01, 0.04), 3)
-        print(start_time, '0', (ppn_start+p), ssd_stride, ssd_mode)
+        print("%.3f %s %ld %d %d" % (start_time, '0', (ppn_start+p), ssd_stride, ssd_mode))
 
-print(start_time + accel_time, 0, 1222321, 1, 1)
+print("%f %s %ld %d %d" % (start_time + accel_time, 0, 1222321, 1, 1))
