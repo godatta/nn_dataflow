35883308ace42440955759cc3e4969ae8b1e786d
msharmavikram <vsm2@illinois.edu>
2018-11-12 20:22:52 -0600

    updated with constant access pattern

--- tracegen/generate_patterns.py
+++ tracegen/generate_patterns.py
@@ -3,8 +3,7 @@ import random as rand
 #Variables:
 
 num_pattern  = 300000 #maximum number of images you can store in 4GB assuming each image is 36KB
-start_time   = 5000.112 #us
-end_time     = 6000 #us
+start_time   = 0 #us
 
 #Accelerator related parameters
 accel_freq   = 10**9     #1GHz
@@ -13,7 +12,7 @@ accel_ccycle = 10000     #compute cycles
 access_pattern = 'constant'    #random - accel_cycle varies in range
 accel_tpc      = 1.0/accel_ccycle # Time per cycle
 
-img_numpages   = 3
+img_numpages   = 1 #imge_pge/stride = 3/3
 
 #SSD Related parameters:
 
@@ -28,15 +27,21 @@ ssd_block_end  = 4096*256 #number of pages - 4 M
 ssd_page_size  = 4096           #in bytes
 ssd_numdev     = 1
 ssd_mode       = 1 #0 is write, 1 is read
-ssd_stride     = 1 #
-arbiter_time   = 0.01 #10ns - 10 cycles
+ssd_stride     = 3 #
+arbiter_time   = 0.00 #10ns - 10 cycles
 
+ppn_start = ssd_block_start
 for i in range(0, num_pattern):
     accel_time = accel_ccycle * accel_tpc
     #print(start_time, '0'),
 
     start_time = start_time + accel_time
-    ppn_start = rand.randint(ssd_block_start, ssd_block_end)
+
+    if access_pattern == 'random':
+        ppn_start = rand.randint(ssd_block_start, ssd_block_end)
+    else:
+        ppn_start = ppn_start + ssd_stride
+
     for p in range(0, img_numpages):
         start_time = round(start_time + rand.uniform(0.01, 0.04), 3)
         print("%.3f %s %ld %d %d" % (start_time, '0', (ppn_start+p), ssd_stride, ssd_mode))
